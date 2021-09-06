504994fe7c5458a29a8407bfdd1a0281eff108a5
artur <test@w3schools.com>
2021-09-06 13:01:48 +0300

    complete second milestone

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import Web3 from 'web3';
 
 
 
+
 class App extends Component {
     
     async componentWillMount() {
@@ -24,12 +25,11 @@ class App extends Component {
     render () {
         return (
             <div>
-                <TopNav clas/>
-
+                <TopNav/>
                 <Tab.Container id="left-tabs-example" defaultActiveKey="first"><br/><br/>
                     <Row>
-                        <Col xs="1">
-                            <Nav variant="pills" className="flex-column" bg="dark">
+                        <Col xs="1" >
+                            <Nav variant="pills" className="flex-column" >
                                 <Nav.Item>
                                     <Nav.Link  eventKey="first"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-graph-up" viewBox="0 0 16 16">
                                                                 <path fillRule="evenodd" d="M0 0h1v15h15v1H0V0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5z"/>
@@ -48,10 +48,10 @@ class App extends Component {
                         <Col xs="11" md={{ span: 9 , offset:1 }}>
                             <Tab.Content>
                                 <Tab.Pane eventKey="first">
-                                     <Display />
+                                     <Display className="bg-dark"/>
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="second">
-                                     <WalletList />
+                                     <WalletList className="bg-dark" />
                                 </Tab.Pane>
                             </Tab.Content>
                         </Col>
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,8 +3,10 @@ import { Button,InputGroup, FormControl} from 'react-bootstrap';
 import './App.css';
 import Web3 from 'web3';
 import { erc20abi , abi } from './abi';
-import { MDBDataTable } from 'mdbreact';
-import { database,  auth } from './firebase/firebase';
+import { MDBDataTableV5 } from 'mdbreact';
+import { database,  } from './firebase/firebase';
+import { FiMonitor , FiPlus , FiCloudLightning ,FiDollarSign } from "react-icons/fi";
+
 
 
 const options = {
@@ -21,11 +23,13 @@ const options = {
   },
 };
 
-const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://purple-wispy-flower.quiknode.pro/a2ae460515f061ce64f526edcb10eda275f62585/', options));
-
+//const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://purple-wispy-flower.quiknode.pro/a2ae460515f061ce64f526edcb10eda275f62585/', options));
+const web3    = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"));
 const uniswap_address = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 const pancake_address = '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F'
 const usdt_address   = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+
+
 class Display extends Component {
     constructor(props){
       super(props)
@@ -39,17 +43,29 @@ class Display extends Component {
         tableDatas : [],
         tableData : [],
         inputAddress : "",
-        tokenAddresses : []
+        tokenAddresses : [],
+        tradetoken : '',
+        tradebuyprice : 0,
+        tradesellprice : 0,
+        traderate : 0,
+        showstate : false,
+        log : ''
       }
     }
 
     async componentWillMount() {
-
         this.loadAddresses()
-    
+        clearInterval(this.timerID);
     }
+
     async componentDidMount() {
-    }
+      this.timerID = setInterval(
+        () => this.start(),
+        10000
+      );
+
+  }
+
     async loadAddresses(){
       database.ref('TokenAddress/').get().then((snapshot) => {
         if (snapshot.exists) {
@@ -73,73 +89,76 @@ class Display extends Component {
     }
 
     async start (){
+      
       for (let index = 0; index < this.state.tokenAddresses.length; index++) {
+        let tokenContract= new web3.eth.Contract(erc20abi,this.state.tokenAddresses[index]["Address"]);
+        let tokenName    = await tokenContract.methods.symbol().call().then(function(res) {  return res;  })
+        let tokenDecimal = await tokenContract.methods.decimals().call()
+    
+        let mycontract1  = new web3.eth.Contract(abi, uniswap_address)
+        let uni_buy      = await mycontract1.methods.getAmountsOut(1000000,[usdt_address,this.state.tokenAddresses[index]["Address"]]).call();
+        let uni_sell     = await mycontract1.methods.getAmountsOut(Math.pow(10, 15), [this.state.tokenAddresses[index]["Address"],usdt_address]).call();
+        uni_buy          = Math.round(Math.pow(10, tokenDecimal+3) /uni_buy[1]) /1000  
+        uni_sell         = Math.round(uni_sell[1]) /1000
+  
+        let mycontract2  = new web3.eth.Contract(abi, pancake_address)
+        let sushi_buy    = await mycontract2.methods.getAmountsOut(1000000 , [usdt_address,this.state.tokenAddresses[index]["Address"]]).call();
+        let sushi_sell   = await mycontract2.methods.getAmountsOut(Math.pow(10, 15) , [this.state.tokenAddresses[index]["Address"],usdt_address]).call();
+        sushi_buy        = Math.round(Math.pow(10, tokenDecimal+3) / sushi_buy[1] ) /1000
+        sushi_sell       = Math.round(sushi_sell[1]) / 1000
+  
+        let uni2sushiRate = Math.round((sushi_sell - uni_buy) * 100000 / sushi_sell) /1000
+        let sushi2uniRate = Math.round(( uni_sell - sushi_buy) * 100000/ uni_sell)/1000
+        let uni2sushiRateStyle 
+        let sushi2uniRateStyle
+
+        if (uni2sushiRate >= 0){
+           uni2sushiRateStyle     = <a className='text-success'> {uni2sushiRate} </a>
+           
+           this.setState({
+             tradetoken : tokenName,
+             tradebuyprice : uni_buy,
+             tradesellprice : sushi_sell,
+             traderate : uni2sushiRate,
+             showstate : true
+           })
+        }
+        else if (uni2sushiRate < 0){
+           uni2sushiRateStyle     = <a className='text-danger'> {uni2sushiRate} </a>
+        }
+  
+        if (sushi2uniRate >= 0){
+           sushi2uniRateStyle     = <a className='text-success'> {sushi2uniRate} </a>
+           this.setState({
+            tradetoken : tokenName,
+            tradebuyprice : sushi_buy,
+            tradesellprice : uni_sell,
+            traderate : sushi2uniRate,
+            showstate : true
+          })
+        }
+        else if (sushi2uniRate < 0){
+           sushi2uniRateStyle     = <a className='text-danger'> {sushi2uniRate} </a>
+        }
+    
+        let tableData = {
+          tokenName     : tokenName,
+          tokenDecimal  : tokenDecimal,
+          uni_buy      : uni_buy,
+          uni_sell      : uni_sell,
+          sushi_buy    : sushi_buy,
+          sushi_sell    : sushi_sell,
+          uni2sushiRate : uni2sushiRate,
+          sushi2uniRate : sushi2uniRate,
+          uni2sushiRateStyle : uni2sushiRateStyle,
+          sushi2uniRateStyle : sushi2uniRateStyle
+        }
+        let tableDatas = this.state.tableDatas
+        tableDatas[index] = tableData
         this.setState({
-            tableDatas : [],
+          tableDatas : tableDatas
         })
-        this.getData(this.state.tokenAddresses[index]["Address"])
       }
-      console.log("start ", this.state.tokenAddresses)
-    }
-
-    async getData (token_address) {
-
-      let tokenContract= new web3.eth.Contract(erc20abi,token_address);
-      let tokenName    = await tokenContract.methods.symbol().call().then(function(res) {  return res;  })
-      let tokenDecimal = await tokenContract.methods.decimals().call()
-    
-    
-      let mycontract1  = new web3.eth.Contract(abi, uniswap_address)
-      let uni_buy      = await mycontract1.methods.getAmountsOut(1000000,[usdt_address,token_address]).call();
-      let uni_sell     = await mycontract1.methods.getAmountsOut(Math.pow(10, 15), [token_address,usdt_address]).call();
-      uni_buy          = Math.round(Math.pow(10, tokenDecimal+3) /uni_buy[1]) /1000  
-      uni_sell         = Math.round(uni_sell[1]) /1000
-
-      let mycontract2  = new web3.eth.Contract(abi, pancake_address)
-      let sushi_buy    = await mycontract2.methods.getAmountsOut(1000000 , [usdt_address,token_address]).call();
-      let sushi_sell   = await mycontract2.methods.getAmountsOut(Math.pow(10, 15) , [token_address,usdt_address]).call();
-      sushi_buy        = Math.round(Math.pow(10, tokenDecimal+3) / sushi_buy[1] ) /1000
-      sushi_sell       = Math.round(sushi_sell[1]) / 1000
-
-      let uni2sushiRate = Math.round((sushi_sell - uni_buy) * 100000 / sushi_sell) /1000
-      let sushi2uniRate = Math.round(( uni_sell - sushi_buy) * 100000/ uni_sell)/1000
-      let uni2sushiRateStyle 
-      let sushi2uniRateStyle
-
-      if (uni2sushiRate >= 0){
-         uni2sushiRateStyle     = <a className='text-success'> {uni2sushiRate} </a>
-      }
-      else if (uni2sushiRate < 0){
-         uni2sushiRateStyle     = <a className='text-danger'> {uni2sushiRate} </a>
-      }
-
-      if (sushi2uniRate >= 0){
-         sushi2uniRateStyle     = <a className='text-success'> {sushi2uniRate} </a>
-      }
-      else if (sushi2uniRate < 0){
-         sushi2uniRateStyle     = <a className='text-danger'> {sushi2uniRate} </a>
-      }
-   
-
-
-      let tableData = {
-        tokenName     : tokenName,
-        tokenDecimal  : tokenDecimal,
-        uni_buy      : uni_buy,
-        uni_sell      : uni_sell,
-        sushi_buy    : sushi_buy,
-        sushi_sell    : sushi_sell,
-        uni2sushiRate : uni2sushiRate,
-        sushi2uniRate : sushi2uniRate,
-        uni2sushiRateStyle : uni2sushiRateStyle,
-        sushi2uniRateStyle : sushi2uniRateStyle
-      }
-
-      let tableDatas = this.state.tableDatas
-      tableDatas.push(tableData)
-      this.setState({
-        tableDatas : tableDatas
-      })
     }
 
     async addAddress(){
@@ -148,12 +167,25 @@ class Display extends Component {
         alert("Please check  Address")
         return
       }
+      for (let index = 0; index < this.state.tokenAddresses.length; index++) {
+        if(this.state.tokenAddresses[index]["Address"] == web3.utils.toChecksumAddress(this.state.inputAddress)){
+          
+          let buffer = ''
+          this.setState({inputAddress : buffer})
+          alert("Aleady exist")
+          return
+        } 
+      }
+
       const tokenAddressList= {
         Address   : web3.utils.toChecksumAddress(this.state.inputAddress),
       }
       var userListRef = database.ref('TokenAddress')
       var newUserRef = userListRef.push();
       newUserRef.set(tokenAddressList);
+      let buffer = ''
+      this.setState({inputAddress : buffer})
+      alert("input successfuly")
       this.loadAddresses();
     }
 
@@ -210,9 +242,8 @@ class Display extends Component {
         
         return (
           <div>
-            <h2>UniSwap SushiSwap Token Price Monitor</h2> <hr/><br/><br/>
-            
-
+            <h2> <FiMonitor/>  UniSwap SushiSwap Token Price Monitor</h2> <hr/><br/>
+            <h5> <FiPlus/>  Add Token Address</h5> <hr/><br/>
             <div className= "row">
               <div className = "col-1"></div>
               <div className = "col-10">
@@ -222,26 +253,40 @@ class Display extends Component {
                     aria-label="Recipient's username"
                     aria-describedby="basic-addon2"
                     defaultValue = {this.state.inputAddress}
+                    value ={this.state.inputAddress}
                     onChange={handleInputAddress}
                   />
                   <Button variant="primary" id="button-addon2"  onClick={()=>this.addAddress()}>
-                    Add Token Address
+                  <FiPlus/>  Add Token Address
                   </Button>
-
                 </InputGroup>
                 </div>
               <div className = "col-1"></div>
             </div>
+            <br/><br/>
+            <h5> <FiCloudLightning/>   Excution Trading</h5> <hr/><br/><br/>
+            <p  show = {this.state.showstate}>We can excute Flash Loan Excute on <b>{this.state.tradetoken}</b> Token, buy price is <b>{this.state.tradebuyprice}USDT</b> , sell price is <b>{this.state.tradesellprice} USDT</b>, profit rate is <b>{this.state.traderate} %</b> </p>
+            <div className= "row">
+            <div className = "col-1"></div>
+            <div className = "col-10">
+            <InputGroup className="mb-3">
+            <FormControl
+                    placeholder="Smart contract address"
+                    aria-label="Smart contract address"
+                    aria-describedby="basic-addon2"
+                    defaultValue = {this.state.contractAddress}
+                  />
+                  <Button variant="primary" id="button-addon2"  onClick={()=>this.addAddress()}>
+                  <FiCloudLightning/> Excute Trading
+                 </Button>
+            </InputGroup>
+            </div></div>
             <br/><br/><br/>
-            <MDBDataTable 
-                    striped
-                    bordered
-                    small
-                    data={
-                        data
-                    }
-            />
+            <h5> <FiDollarSign/>  Buy and Sell token price table</h5> <hr/>
+            <MDBDataTableV5 hover entriesOptions={[5, 20, 25]} entries={5} pagesAmount={4} data={data} />
           </div>
+
+          
         );
     }
 }
